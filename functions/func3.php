c7620dfef5676ebf9ebec365fffa9a65f886b76e
James <james@WenjiedeMini.xqgw.xq.cn>
2016-07-20 12:26:56 +0800

    update fun3.php file

--- functions/func3.php
+++ functions/func3.php
@@ -6,6 +6,9 @@
  *  @author: Jamesxu
  *  @email: smelikecat@163.com
 */
+
+
+// ## Server ###
 // set some variables
 $host = "127.0.0.1";
 $port = 25003;
@@ -31,4 +34,23 @@ $output = strrev($input) . "\n";
 socket_write($spawn, $output, strlen ($output)) or die("Could not write output\n");
 // close sockets
 socket_close($spawn);
+socket_close($socket);
+
+
+// ### Client ###
+
+$host    = "127.0.0.1";
+$port    = 25003;
+$message = "Hello Server client 1";
+echo "Message To server :".$message;
+// create socket
+$socket = socket_create(AF_INET, SOCK_STREAM, 0) or die("Could not create socket\n");
+// connect to server
+$result = socket_connect($socket, $host, $port) or die("Could not connect to server\n");
+// send string to server
+socket_write($socket, $message, strlen($message)) or die("Could not send data to server\n");
+// get server response
+$result = socket_read ($socket, 1024) or die("Could not read server response\n");
+echo "Reply From Server  :".$result;
+// close socket
 socket_close($socket);
